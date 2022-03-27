cffd651dfe8138729b0f7e377cce3b4d6c152a8e
Роман Роман <romanbindyug@yandex.ru>
2022-03-28 01:37:19 +0300

    Добавьте файлы проекта.

---
+++ WpfLab7_1/MyCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfLab7_1
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Close { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection close = new InputGestureCollection();
+            close.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Close = new RoutedUICommand("Закрыть", "Close", typeof(MyCommands), close);
+        }
+    }
+}
